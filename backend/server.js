34332943db322d7490ab0671e15feb2b3ceb6ea4
Pierre van Zyl <pierrevanzyl777@gmail.com>
2022-07-20 16:53:59 +0200

    connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express');
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 
@@ -18,7 +19,13 @@ app.use((req, res, next)=>{
 // routes
 app.use('/api/workouts/', workoutRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, ()=>{
-  console.log('Listening on port ' + process.env.PORT)
+// connect to db
+// ### Remember that the ip address allocation must be changed if your ip changes at mongodb network access
+mongoose.connect(process.env.MONGO_URI)
+  .then(()=>{
+    // listen for requests
+  app.listen(process.env.PORT, ()=>{
+  console.log('Connected to db and Listening on port ' + process.env.PORT)
 })
+  })
+  .catch((error)=>{console.log(error)})
