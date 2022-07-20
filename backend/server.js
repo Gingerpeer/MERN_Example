9d1f2215b5016da569dd5991b23eb1337e894733
Pierre van Zyl <pierrevanzyl777@gmail.com>
2022-07-20 16:20:43 +0200

    express app created

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ const express = require('express');
 // express app call
 const app = express()
 
+app.use((req, res, next)=>{
+  console.log(req.path, req.method)
+  next()
+})
 // routes
 app.get('/', (req,res)=>{
   res.json({msg: 'welcome to the app'})
