c8edfeb37feced073031df93f712165605b8efae
Pierre van Zyl <pierrevanzyl777@gmail.com>
2022-07-20 17:09:43 +0200

    models and schema done

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+// schema defines how the docs must look
+const workoutSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  reps: {
+    type: Number,
+    required: true
+  },
+  load: {
+    type: Number,
+    required: true
+  }
+},{ timestamps: true })
+// models gives us access and in this case the model is called 'Workout'
+module.exports = mongoose.model('Workout', workoutSchema)
