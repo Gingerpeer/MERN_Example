7aa10066aba687b7fc176f5402735ece7e76c128
Pierre van Zyl <pierrevanzyl777@gmail.com>
2022-07-22 16:45:57 +0200

    requireAuth middleware created in workouts

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -6,9 +6,11 @@ const {
   deleteWorkout,
   updateWorkout
     } = require('../controllers/workoutController')
+const requireAuth = require('../middleware/requireAuth')
 
 const router = express.Router()
-
+// require auth for all workouts routes
+router.use(requireAuth)
 // GET all workouts
 router.get('/', getWorkouts)
 
