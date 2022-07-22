309493ba666407fb31f59a22cd629223b2552ed3
Pierre van Zyl <pierrevanzyl777@gmail.com>
2022-07-22 17:22:31 +0200

    conditional visibility for workouts done

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -2,8 +2,11 @@ const Workout = require('../models/workoutModel')
 const mongoose = require('mongoose')
 // get all workouts 
 const getWorkouts = async(req,res)=>{
+  const user_id = req.user._id
   // sorted newest top
-  const workouts = await Workout.find({}).sort({createdAt: -1})
+  const workouts = await Workout.find({ user_id }).sort({createdAt: -1})
+  
+
   res.status(200).json(workouts)
 }
 // get a single workout
@@ -43,7 +46,8 @@ const createWorkout = async (req,res) =>{
 
   // add doc to db
   try{
-    const workout = await Workout.create({title, load, reps})
+    const user_id = req.user._id
+    const workout = await Workout.create({title, load, reps, user_id})
     res.status(200).json(workout)
   } catch(error){
     res.status(400).json({error: error.message})
