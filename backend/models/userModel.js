68399ef5827423bf014e76a7021dfd60fbb0c534
Pierre van Zyl <pierrevanzyl777@gmail.com>
2022-07-22 15:23:01 +0200

    login functionality created with jwt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -47,4 +47,26 @@ userSchema.statics.signup = async function(email,password){
 
 }
 
+// static login method
+userSchema.statics.login = async function(email,password){
+  // validation
+  if(!email || !password){
+    throw Error('All fields must be filled out')
+  }
+  const user = await this.findOne({ email })
+
+  if(!user){
+    throw Error('Incorrect Email')
+  }
+
+  const match = await bcrypt.compare(password, user.password)
+
+  if(!match){
+    throw Error('Incorrect Password')
+  }
+
+  return user
+  
+}
+
 module.exports = mongoose.model('User', userSchema)
